5bb81bd5a3f92fe31bf9d457d12802b3e6b7f7da
yenna <yena.gwak@gmail.com>
2025-07-23 13:34:04 +0900

    [refactoring] refactor guess method

--- TDD2_Baseball/baseball.cpp
+++ TDD2_Baseball/baseball.cpp
@@ -6,16 +6,24 @@ using namespace std;
 class Baseball {
 public:
 	void guess(const string& guessNum) {
+		assertIllegalArgument(guessNum);
+	}
+
+	void assertIllegalArgument(const std::string& guessNum){
 		if (guessNum.length() != 3)
 			throw length_error("Must be three letters.\n");
 
 		for (char ch : guessNum) {
-			if (ch < '0' || ch > '9')
-				throw invalid_argument("Must be number.\n");
+			if (ch >= '0' && ch <= '9') continue;
+			throw invalid_argument("Must be number.\n");
 		}
-		if(guessNum[0] == guessNum[1] 
-			|| guessNum[1] == guessNum[2]
-			|| guessNum[2] == guessNum[0])
+		if (isDuplicatedNumber(guessNum))
 			throw invalid_argument("Must not have the same number.\n");
+	}
+
+	bool isDuplicatedNumber(const std::string& guessNum){
+		return guessNum[0] == guessNum[1]
+			|| guessNum[1] == guessNum[2]
+			|| guessNum[2] == guessNum[0];
 	}
 };
